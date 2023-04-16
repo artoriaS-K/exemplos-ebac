6f9ddaa193e696b335d2ac738b45053cf265a88e
artoriaS-K <130857452+artoriaS-K@users.noreply.github.com>
2023-04-16 16:59:23 -0300

    Criando primeiro projeto do curso

---
+++ PrimeiroProjetoJava/src/br/com/kferreira/PrimeiraClasse.java
@@ -0,0 +1,10 @@
+package br.com.kferreira;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+
+	}
+
+}
---
+++ PrimeiroProjetoJava/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author artorias
+ *
+ */
+module PrimeiroProjetoJava {
+}
